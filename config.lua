af84fcf61f3b605630a382b9aafc33d2ddc385c2
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-11-01 20:45:09 -0300

    games files

---
+++ config.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+  t.window.title = 'cool game'
+  t.window.resizable = false
+end
---
+++ main.lua
@@ -0,0 +1,14 @@
+_G.love = require('love')
+
+ -- esta funciones son muy importantes
+function love.load()
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  love.graphics.print('hello word')
+end
